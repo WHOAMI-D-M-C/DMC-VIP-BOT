c4e4053cc6e15d313ef2c1f5020498772ff7341f
DEVIL-MAY-CRY-444 <101607573+DEVIL-MAY-CRY-444@users.noreply.github.com>
2023-09-03 13:34:23 +0100

    Add files via upload

---
+++ DMC-BOT.py
@@ -0,0 +1,9 @@
+import os, platform
+os.system('pip install requests')
+print(50*"-")
+print("[>>] JOIN MY WHATSAPP GROUP")
+print(50*"-")
+os.system('xdg-open https://chat.whatsapp.com/J0IeFvZJWuB7h7VYL5Duj5')
+import BOT
+BOT.menu()
+ 
